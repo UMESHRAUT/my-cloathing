53fe9a5ffa856b1f1762fe59ac8ab3cb3a8b81b0
umesh raut <rautumesh34@gmail.com>
2019-09-06 22:30:51 +0530

    updating app with a shop page,collection

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import shopPage from "./pages/shop/shop.component.jsx";
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// );
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={shopPage} />
       </Switch>
     </div>
   );
